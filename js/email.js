823c593d5b04d38c9d30b4ba155697db600e3a68
sergio <sergiowani@gmail.com>
2024-04-04 13:07:22 +0200

    formulario

--- js/email.js
+++ js/email.js
@@ -3,14 +3,13 @@ document.getElementById('formularioContacto').addEventListener('submit', functio
 
   const nombre = document.getElementById('nombre').value;
   const asunto = document.getElementById('asunto').value;
-  const email = document.getElementById('email').value;
   const mensaje = document.getElementById('mensaje').value;
 
   //Dirección de correo electrónico
   const destinatario = 'sergiowani@outlook.es';
   
   //Enlace 'mailto' con los valores recopilados
-  const mailtoLink = 'mailto:' + destinatario + '?subject=' + encodeURIComponent(asunto) + '&body=' + encodeURIComponent('Nombre: ' + nombre + '\nCorreo electrónico: ' + email + '\n\nMensaje:\n' + mensaje);
+  const mailtoLink = 'mailto:' + destinatario + '?subject=' + encodeURIComponent(asunto) + '&body=' + encodeURIComponent('Nombre: ' + nombre + '\n\nMensaje:\n' + mensaje);
 
   //Abrir el enlace 'mailto'
   window.location.href = mailtoLink;
