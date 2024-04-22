61a6e6e2ddd6c6c123bdc4ca32ddc4abf862c2f1
sergio <sergiowani@gmail.com>
2024-04-22 13:07:44 +0200

    seo

--- js/navBar.js
+++ js/navBar.js
@@ -14,8 +14,22 @@ document.addEventListener('DOMContentLoaded', function () {
   let navListItems = document.querySelectorAll('.nav-links li');
 
   navListItems.forEach(function (item) {
-    item.addEventListener('click', function () {
+    item.addEventListener('click', function (event) {
+      // Prevenir el comportamiento predeterminado del enlace
+      event.preventDefault();
+      
+      // Obtener el hash del enlace
+      let hash = item.querySelector('a').getAttribute('href');
+
+      // Cerrar el menú
       navLinks.classList.remove('show');
+
+      // Ir directamente a la sección correspondiente sin scroll
+      if (hash && document.querySelector(hash)) {
+        document.querySelector(hash).scrollIntoView({
+          behavior: 'auto'
+        });
+      }
     });
   });
 });
